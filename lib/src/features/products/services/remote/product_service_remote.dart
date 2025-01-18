2f465445e1cd3775d8dfbc5abc65e2e34943ddef
barzi-yassin <barzy.nettwork@gmail.com>
2025-01-18 11:09:03 +0300

    .

--- lib/src/features/products/services/remote/product_service_remote.dart
+++ lib/src/features/products/services/remote/product_service_remote.dart
@@ -9,12 +9,17 @@ class ProductServiceRemote {
   static Future<ProductPack?> get fetchProducts async {
     ProductPack? output;
     try {
+      debugPrint('1');
       final http.Response response = await http.get(Uri.parse(_url));
+      debugPrint('2');
       if (response.statusCode == 200) {
         output = ProductPack.fromJson(response.body);
+        debugPrint('ssssssssssssss response.body::::: ${response.body}');
+        debugPrint('ssssssssssssss output :::: ${output}');
       } else {
         debugPrint('HTTP response status code is not 200!');
       }
+      debugPrint('3');
     } catch (e) {
       debugPrint(
           'Something went wrong while fetching products from an API call:');
